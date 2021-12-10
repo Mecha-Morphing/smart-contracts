f22e82de80edc351bdd6d913e69f87a12c79d833
Mecha-Morphing <95901467+Mecha-Morphing@users.noreply.github.com>
2021-12-10 14:02:41 +0800

    Create MechaMorphingCoin.sol

---
+++ MechaMorphingCoin.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MechaMorphingCoin is ERC20 {
+    constructor() ERC20("MechaMorphingCoin", "MMC") {
+        _mint(msg.sender, 1000000000 * 10**uint(decimals()));
+    }
+}
